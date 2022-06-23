12ed0493e17ffb9790c0b05f91a6e97625b081e9
dc_pushpa <pushpa@sajhainvestment.com>
2022-06-23 09:08:59 +0545

    outlet-with-context

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,26 +15,9 @@ import {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements AfterViewInit {
-  title = 'login';
-  user: any;
-
-  component!: ComponentRef<AuthFormComponent>;
-
-  @ViewChild('entry', { read: ViewContainerRef }) entry!: ViewContainerRef;
-  @ViewChild('tmpl') tmpl!: TemplateRef<any>;
-
-  constructor() {}
-
-  ngAfterViewInit(): void {
-    this.entry.createEmbeddedView(this.tmpl, {
-      $implicit: 'Pushpa Raj Dangi',
-      location: 'Kathmandu Nepal',
-    });
-  }
-
-  login(user: any) {
-    console.log(this.user);
-    this.user = user;
-  }
+export class AppComponent {
+  ctx = {
+    $implicit: 'Pushpa DC',
+    location: 'Kathmandu,Nepal',
+  };
 }
